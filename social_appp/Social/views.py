17cc6ef56060a28eedf1358118ba6dfefad77676
NMShihab <nmshihabislam@gmail.com>
2020-07-26 14:35:34 +0600

    Home_page

--- social_appp/Social/views.py
+++ social_appp/Social/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from django.shortcuts import  HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from Login.models import UserProfile
+from django.contrib.auth.models import  User
+
 # Create your views here.
 
 @login_required
 
 def home (request):
 
-    return render(request,'Social/home.html',context={'title':'Home'})
+    if request.method == 'GET':
+        search = request.GET.get('search','')
+        result = User.objects.filter(username__icontains = search)
+
+    return render(request,'Social/home.html',context={'title':'Home',"search":search,"result":result})
