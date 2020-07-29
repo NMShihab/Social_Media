5c0b36f1f0230d6d3f66bf8befbb52d7b37aa93c
NMShihab <nmshihabislam@gmail.com>
2020-07-29 09:05:12 +0600

    display posts

--- social_appp/Social/views.py
+++ social_appp/Social/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render
 from django.shortcuts import  HttpResponse
 from django.contrib.auth.decorators import login_required
 
-from Login.models import UserProfile
+from Login.models import UserProfile , Follow
 from django.contrib.auth.models import  User
+from Social.models import Post
 
 # Create your views here.
 
 @login_required
-
 def home (request):
+
+    following_list = Follow.objects.filter(follower= request.user)
+
+    posts = Post.objects.filter(author__in = following_list.values_list('following'))
 
     if request.method == 'GET':
         search = request.GET.get('search','')
         result = User.objects.filter(username__icontains = search)
 
-    return render(request,'Social/home.html',context={'title':'Home',"search":search,"result":result})
+    return render(request,'Social/home.html',context={'title':'Home',"search":search,"result":result,
+    'posts':posts})
+
