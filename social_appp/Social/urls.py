4419e8943b31a3ec8f874ca269a5ef19849f82f8
NMShihab <nmshihabislam@gmail.com>
2020-07-30 10:17:09 +0600

    Liked , Unliked

--- social_appp/Social/urls.py
+++ social_appp/Social/urls.py
@@ -5,5 +5,7 @@ app_name = "Social"
 
 urlpatterns = [
     path('home/',views.home,name='home'),
+    path('liked/<pk>',views.liked_post,name='liked'),
+    path('unliked/<pk>',views.unliked_post,name='unliked')
 
 ]
--- social_appp/Social/views.py
+++ social_appp/Social/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.shortcuts import  HttpResponse
+from django.shortcuts import render ,  HttpResponse ,HttpResponseRedirect
+from django.urls import  reverse
 from django.contrib.auth.decorators import login_required
 
 from Login.models import UserProfile , Follow
 from django.contrib.auth.models import  User
-from Social.models import Post
+from Social.models import Post,Like
 
 # Create your views here.
 
@@ -12,13 +12,35 @@ from Social.models import Post
 def home (request):
 
     following_list = Follow.objects.filter(follower= request.user)
-
     posts = Post.objects.filter(author__in = following_list.values_list('following'))
+
+    liked_post = Like.objects.filter(user=request.user)
+    liked_post_list = liked_post.values_list('post',flat=True)
 
     if request.method == 'GET':
         search = request.GET.get('search','')
         result = User.objects.filter(username__icontains = search)
 
     return render(request,'Social/home.html',context={'title':'Home',"search":search,"result":result,
-    'posts':posts})
+    'posts':posts,'liked_post_list':liked_post_list})
+
+
+@login_required
+def liked_post(request,pk):
+    post = Post.objects.get(pk=pk)
+    
+    already_liked = Like.objects.filter(post=post,user=request.user)
+
+    if not already_liked:
+        liked_post = Like(post=post,user=request.user)
+        liked_post.save() 
+    return HttpResponseRedirect(reverse('Social:home'))
+
+@login_required
+def unliked_post (request,pk):
+    post = Post.objects.get(pk=pk)
+    user = request.user
+    already_liked = Like.objects.filter(post=post,user=user)
+    already_liked.delete()
+    return HttpResponseRedirect(reverse('Social:home'))
 
